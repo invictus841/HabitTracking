89cc236de38a68b76fbd004737495a6bf03e90aa
NT <usedforfunny@gmail.com>
2022-07-24 11:57:27 +0200

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Alexandre Talatinian on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
